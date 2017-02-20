5350460346754095ff428c56b54dd25e2a7ac415
samvelie <sam.velie@gmail.com>
2017-02-20 13:45:58 -0600

    page updating dynamically to meet requirements

--- client.js
+++ client.js
@@ -1,23 +1,21 @@
-var totalRed = 0;
-var totalYellow = 0;
-var totalGreen = 0;
-var totalBlue = 0;
-
 $(document).ready(function(){
 
-$('button').on('click', function(){
-  var $clickedButtonColor = $(this).data('color');
-  console.log($clickedButtonColor);
+  //listens for clicks on all page buttons
+  $('button').on('click', function(){
 
-  $('.container').append('<div class="color-cube '+ $clickedButtonColor +'"></div>');
+    //assigns the value of the clicked button's data-color attribute to a new variable called $clickedButtonColor
+    var $clickedButtonColor = $(this).data('color');
 
-  if($clickedButtonColor=='red'){
-    totalRed++;
-    $('#red').text(totalRed);
-  }
+    //uses the $clickedButtonColor value to find the id matching that value on the page, and picks up the text there (showing the total), converting it to a number
+    var $totalCounter = Number($('#' + $clickedButtonColor).text());
 
-});
+    //increments the converted number by 1
+    $totalCounter++;
 
+    //places the updated number as text back into the page at the spot where it was found
+    $('#' + $clickedButtonColor).text($totalCounter);
 
-
-});
+    //adds a div with the class color-cube and the matching class to the color of the button clicked
+    $('.container').append('<div class="color-cube '+ $clickedButtonColor +'"></div>');
+  });//end button click listener function
+}); //end document ready
